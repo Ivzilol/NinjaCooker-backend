3b5f077b7f57856037cf9ef8df92de7bcf8ec516
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-07 19:33:29 +0200

    Initial Commit

---
+++ src/main/java/com/example/ninjacookerbackend/NinjaCookerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ninjacookerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaCookerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjaCookerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ninjacookerbackend/NinjaCookerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ninjacookerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaCookerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
