4b7c4ad3e93e3956a9f3bf3f6bb829366db99886
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-08 13:43:19 +0200

    Create entity

---
+++ src/main/java/com/example/ninjacookerbackend/util/JwtService.java
@@ -0,0 +1,80 @@
+package com.example.ninjacookerbackend.util;
+
+import com.example.ninjacookerbackend.models.auth.NinjaUserDetails;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    public static final long JWT_TOKEN_VALIDITY = 7 * 24 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (userDetails != null && username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    private Boolean ignoreTokenExpiration(String token) {
+        return null;
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, NinjaUserDetails userDetails) {
+        extraClaims.put("authorities",
+                userDetails
+                        .getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .toList());
+        return doGenerateToken(extraClaims, userDetails);
+    }
+
+    private String doGenerateToken(Map<String, Object> extraClaims, NinjaUserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setId(String.valueOf(userDetails.getId()))
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * JWT_TOKEN_VALIDITY))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+}
