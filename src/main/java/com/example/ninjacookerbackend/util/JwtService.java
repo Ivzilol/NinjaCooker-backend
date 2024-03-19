e7d13aca7cd774dbb52f57a71f7adf4ef8b4031e
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-19 10:24:42 +0200

    Create entity

--- src/main/java/com/example/ninjacookerbackend/util/JwtService.java
+++ src/main/java/com/example/ninjacookerbackend/util/JwtService.java
@@ -66,6 +66,7 @@ public class JwtService {
                         .toList());
         return doGenerateToken(extraClaims, userDetails);
     }
+
     private String doGenerateToken(Map<String, Object> extraClaims, NinjaUserDetails userDetails) {
         return Jwts.builder()
                 .setClaims(extraClaims)
