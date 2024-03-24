94a323fe949290a73cc392ae17ea9823d0d34520
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-24 15:34:48 +0200

    Create entity

--- src/main/java/com/example/ninjacookerbackend/util/JwtService.java
+++ src/main/java/com/example/ninjacookerbackend/util/JwtService.java
@@ -25,7 +25,6 @@ public class JwtService {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
-
     private Claims getAllClaimsFromToken(String token) {
         return Jwts.parser()
                 .setSigningKey(secret)
