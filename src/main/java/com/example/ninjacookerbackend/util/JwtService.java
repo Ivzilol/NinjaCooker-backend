64a79f4769e1f35ea07e9db05f90bf30c14969c2
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-24 15:34:40 +0200

    Create entity

--- src/main/java/com/example/ninjacookerbackend/util/JwtService.java
+++ src/main/java/com/example/ninjacookerbackend/util/JwtService.java
@@ -21,7 +21,6 @@ public class JwtService {
     public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
-
     public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
